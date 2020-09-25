5945e9140f564adf92828955fc2561b44fc289e1
Ishaan <ishaan.m@tyroo.com>
2020-09-25 18:55:47 +0530

    deploy

---
+++ build/precache-manifest.678a02661eaa5cd9efe5bca9f9706d11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "637d44db5cf02dcda49c450d232c6bf7",
+    "url": "/react-gmail-logo/index.html"
+  },
+  {
+    "revision": "9601fd1cdbc595cd8c2e",
+    "url": "/react-gmail-logo/static/css/main.52dc506e.chunk.css"
+  },
+  {
+    "revision": "3f86a573e8cb0ab5264d",
+    "url": "/react-gmail-logo/static/js/2.77a3806e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gmail-logo/static/js/2.77a3806e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9601fd1cdbc595cd8c2e",
+    "url": "/react-gmail-logo/static/js/main.2ee7c6c4.chunk.js"
+  },
+  {
+    "revision": "e2000fbb333fdb37c84a",
+    "url": "/react-gmail-logo/static/js/runtime-main.f9fe0b9a.js"
+  }
+]);
