8ae678080d038ec268152f721d8a8a1e1f951c0b
Mayur Panchal <mayurpanchal002@gmail.com>
2020-10-03 17:28:39 +0530

    added dark theme support

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
-import React from "react";
-import "./App.css";
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import moon from './Assets/moon.svg';
+import sun from './Assets/sun.svg';
 
 function App() {
-  return <div className="card"></div>;
+	const [isDark, setDark] = useState(false);
+
+	useEffect(() => {
+		if (isDark) {
+			document.querySelector('body').classList.remove('light');
+			document.querySelector('body').classList.add('dark');
+		} else {
+			document.querySelector('body').classList.remove('dark');
+			document.querySelector('body').classList.add('light');
+		}
+	}, [isDark]);
+
+	return (
+		<>
+			<button
+				onClick={() => setDark((prevProp) => !prevProp)}
+				className='theme-container'
+			>
+				<img
+					className='theme-icon'
+					src={isDark ? sun : moon}
+					alt='theme toggle icon'
+				/>
+			</button>
+			<div className='card'></div>
+		</>
+	);
 }
 
 export default App;
